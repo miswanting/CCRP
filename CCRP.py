1b97e7f6dead2c8e73aec349e988cde4bb12f434
何雨航 <heyuhang987@gmail.com>
2017-03-18 20:27:13 +0800

    稳定

--- CCRP.py
+++ CCRP.py
@@ -1,14 +1,20 @@
 # coding=utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def home():
     a = {}
-    a['href'] = 'CCRP.py'
+    a['href'] = 'cmd/scan_wifi'
     a['caption'] = '扫描无线热点'
     return render_template('home.html', item=a)
+
+
+@app.route("/cmd/<cmd>")
+def do_cmd(cmd):
+    print(cmd)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
