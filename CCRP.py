e660ce068430ffe9ab2e573a1689f81cb04f0a3e
何雨航 <heyuhang987@gmail.com>
2017-03-18 22:02:29 +0800

    update

--- CCRP.py
+++ CCRP.py
@@ -8,6 +8,14 @@ main['main_text'] = ''
 
 @app.route("/")
 def home():
+    main['cmds'] = [
+        {'href': 'cmd/scan_wifi', 'text': '扫描无线热点'},
+        {'href': 'cmd/test', 'text': '测试'},
+        {'href': 'cmd/test', 'text': '测试'},
+        {'href': 'cmd/test', 'text': '测试'},
+        {'href': 'cmd/test', 'text': '测试'}
+    ]
+
     main['href'] = 'cmd/scan_wifi'
     main['caption'] = '扫描无线热点'
     return render_template('home.html', item=main)
@@ -15,15 +23,23 @@ def home():
 
 @app.route("/cmd/<cmd>")
 def do_cmd(cmd):
+    main['main_text'] = ''
     print(cmd)
     exe_cmd(cmd)
     return redirect('/')
 
 
 def exe_cmd(cmd):
-    for each in Cell.all('wlan0'):
-        print(each.ssid)
-        main['main_text'] += each.ssid + '\n'
+    if cmd == 'scan_wifi':
+        for each in Cell.all('wlan0'):
+            print(each.ssid)
+            main['main_text'] += each.ssid + '\n'
+        print(main['main_text'])
+    elif cmd == 'test':
+        for each in range(0, 10):
+            print(each)
+            main['main_text'] += str(each) + '\n'
+        print(main['main_text'])
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
