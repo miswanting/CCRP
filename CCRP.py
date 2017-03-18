bd4698f5c7399b2871ab7dcbce247cabfab0e79a
何雨航 <heyuhang987@gmail.com>
2017-03-18 20:33:25 +0800

    try

--- CCRP.py
+++ CCRP.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from wifi import Cell, Scheme
 from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
@@ -14,7 +15,12 @@ def home():
 @app.route("/cmd/<cmd>")
 def do_cmd(cmd):
     print(cmd)
+    self.exe_cmd(cmd)
     return redirect('/')
+
+
+def exe_cmd(cmd):
+    Cell.all('wlan0')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
