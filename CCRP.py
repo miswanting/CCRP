d6dc1ff10490d7af692432aae0e13cd1fccc28de
何雨航 <heyuhang987@gmail.com>
2017-03-18 20:09:34 +0800

    try

--- CCRP.py
+++ CCRP.py
@@ -1,11 +1,14 @@
 # coding=utf-8
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    a = {}
+    a['href'] = 'CCRP.py'
+    a['caption'] = '扫描无线热点'
+    return render_template('home.html', item=a)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
